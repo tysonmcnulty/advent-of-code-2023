54239dc1272eaa5bed28f3c31981d2064bf176ef
Tyson McNulty <tyson.mcnulty@gmail.com>
2023-12-03 19:47:58 -0500

    Add solutions

--- src/day01/__init__.py
+++ src/day01/__init__.py
@@ -1,10 +1,46 @@
+import re
 from pathlib import Path
+from enum import Enum
 
 
-def load(data_file: Path):
+DIGIT_MAP = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+class Strategy(Enum):
+    DIGITS_ONLY = r"(?=(\d))"
+    DIGITS_OR_WORDS = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
+
+
+def load(data_file: Path, strategy: Strategy = Strategy.DIGITS_ONLY):
     calibration_values = []
+
     with open(data_file) as calibration_data:
         for line in calibration_data:
-            pass
+            digits = parse_digits(line, strategy)
+            calibration_values.append(digits[0] * 10 + digits[-1])
 
     return calibration_values
+
+
+def parse_digits(line: str, strategy: Strategy):
+    return list(map(lambda s: DIGIT_MAP[s], re.findall(strategy.value, line)))
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,14 +1,37 @@
 import unittest
 from pathlib import Path
 
-from src.day01 import load
+from src.day01 import load, Strategy, parse_digits
 
 
 class Day01Tests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.example = load(Path(__file__).parent / "resources/day01/example.txt")
+        cls.example_2 = load(
+            Path(__file__).parent / "resources/day01/example2.txt",
+            Strategy.DIGITS_OR_WORDS,
+        )
         cls.input = load(Path(__file__).parent / "../src/day01/input.txt")
+        cls.input_2 = load(
+            Path(__file__).parent / "../src/day01/input.txt", Strategy.DIGITS_OR_WORDS
+        )
 
     def test_load_example(self):
         self.assertEqual(self.example, [12, 38, 15, 77])
+        self.assertEqual(sum(self.example), 142)
+
+    def test_load_example2(self):
+        self.assertEqual(self.example_2, [29, 83, 13, 24, 42, 14, 76])
+        self.assertEqual(sum(self.example_2), 281)
+
+    def test_parse_line(self):
+        self.assertEqual(
+            parse_digits("12threeight\n", Strategy.DIGITS_OR_WORDS), [1, 2, 3, 8]
+        )
+
+    def test_solution_1(self):
+        self.assertEqual(sum(self.input), 55090)
+
+    def test_solution_2(self):
+        self.assertEqual(sum(self.input_2), 54871)
