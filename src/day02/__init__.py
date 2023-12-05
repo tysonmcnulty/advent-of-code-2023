e7a1bcbe2864f20f6f0abd8ee8041439f35020a2
Tyson McNulty <tyson.mcnulty@gmail.com>
2023-12-04 20:33:40 -0500

    Day 2 green

--- src/day02/__init__.py
+++ src/day02/__init__.py
@@ -1,6 +1,7 @@
 import re
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Self
 
 
 def load(data_file: Path):
@@ -13,17 +14,31 @@ def load(data_file: Path):
 
 
 @dataclass
+class Bag:
+    blue: int = 0
+    green: int = 0
+    red: int = 0
+
+    @property
+    def power(self) -> int:
+        return self.blue * self.green * self.red
+
+
+@dataclass
 class Drawing:
     blue: int = 0
     green: int = 0
     red: int = 0
 
     @staticmethod
-    def from_str(drawing_data: str):
+    def from_str(drawing_data: str) -> Self:
         drawing_kwargs = dict(
             (k, int(v)) for v, k in re.findall(r"(\d+) (blue|green|red)", drawing_data)
         )
         return Drawing(**drawing_kwargs)
+
+    def is_possible(self, bag) -> bool:
+        return self.blue <= bag.blue and self.green <= bag.green and self.red <= bag.red
 
 
 @dataclass
@@ -32,8 +47,19 @@ class Game:
     drawings: list[Drawing] = field(default_factory=list)
 
     @staticmethod
-    def from_str(game_data: str):
+    def from_str(game_data: str) -> Self:
         (id_data, drawings_data) = game_data.split(":")
         id = int(re.search(r"Game (\d+)", id_data).group(1))
         drawings = [Drawing.from_str(d) for d in drawings_data.split(";")]
         return Game(id, drawings)
+
+    @property
+    def smallest_possible_bag(self) -> Bag:
+        return Bag(
+            red=max(d.red for d in self.drawings),
+            blue=max(d.blue for d in self.drawings),
+            green=max(d.green for d in self.drawings),
+        )
+
+    def is_possible(self, bag: Bag) -> bool:
+        return all(d.is_possible(bag) for d in self.drawings)
--- tests/test_day02.py
+++ tests/test_day02.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 
-from src.day02 import load, Drawing, Game
+from src.day02 import load, Drawing, Game, Bag
 
 
 class Day02Tests(unittest.TestCase):
@@ -12,13 +12,57 @@ class Day02Tests(unittest.TestCase):
 
     def test_load_example(self):
         self.assertEqual(
-            self.example[0],
-            Game(
-                1,
-                [
-                    Drawing(blue=3, red=4),
-                    Drawing(red=1, green=2, blue=6),
-                    Drawing(green=2),
-                ],
-            ),
+            self.example[0:2],
+            [
+                Game(
+                    1,
+                    [
+                        Drawing(blue=3, red=4),
+                        Drawing(red=1, green=2, blue=6),
+                        Drawing(green=2),
+                    ],
+                ),
+                Game(
+                    2,
+                    [
+                        Drawing(blue=1, green=2),
+                        Drawing(green=3, blue=4, red=1),
+                        Drawing(green=1, blue=1),
+                    ],
+                ),
+            ],
         )
+
+    def test_game_is_possible_for_bag(self):
+        bag = Bag(red=12, green=13, blue=14)
+        self.assertEqual(
+            [True, True, False, False, True],
+            [game.is_possible(bag) for game in self.example],
+        )
+        self.assertEqual(
+            8, sum(game.id for game in self.example if game.is_possible(bag))
+        )
+
+    def test_solution_1(self):
+        bag = Bag(red=12, green=13, blue=14)
+        self.assertEqual(
+            2512, sum(game.id for game in self.input if game.is_possible(bag))
+        )
+
+    def test_smallest_possible_bag(self):
+        self.assertEqual(
+            [
+                Bag(blue=6, green=2, red=4),
+                Bag(blue=4, green=3, red=1),
+                Bag(blue=6, green=13, red=20),
+                Bag(blue=15, green=3, red=14),
+                Bag(blue=2, green=3, red=6),
+            ],
+            [game.smallest_possible_bag for game in self.example],
+        )
+
+    def test_power(self):
+        self.assertEqual(2286, sum(game.smallest_possible_bag.power for game in self.example))
+
+    def test_solution_2(self):
+        self.assertEqual(67335, sum(game.smallest_possible_bag.power for game in self.input))
