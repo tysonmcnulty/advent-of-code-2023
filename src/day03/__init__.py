05df025ec863528f51d5a097e7f4e8d27eb18449
Tyson McNulty <tyson.mcnulty@gmail.com>
2023-12-05 20:49:27 -0500

    Bootstrapping concepts for solution 1

--- src/day03/__init__.py
+++ src/day03/__init__.py
@@ -1,12 +1,113 @@
 import re
+from collections.abc import Iterable
 from dataclasses import dataclass, field
+from functools import cached_property
 from pathlib import Path
 from typing import Self
 
 
-def load(data_file: Path):
-    with open(data_file) as lines:
-        for line in lines:
-            pass
+@dataclass(frozen=True)
+class Coordinate:
+    x: int
+    y: int
 
-    return []
+
+@dataclass(frozen=True)
+class Extent:
+    top_left: Coordinate
+    bottom_right: Coordinate
+
+    @cached_property
+    def neighborhood(self) -> "Neighborhood":
+        return Neighborhood(inner=self)
+
+    def __contains__(self, item: Coordinate) -> bool:
+        return (
+            self.top_left.x <= item.x
+            and item.x <= self.bottom_right.x
+            and self.top_left.y <= item.y
+            and item.y <= self.bottom_right.y
+        )
+
+    def __iter__(self) -> Iterable[Coordinate]:
+        return (
+            Coordinate(x, y)
+            for x in range(self.top_left.x, self.bottom_right.x + 1)
+            for y in range(self.top_left.y, self.bottom_right.y + 1)
+        )
+
+    def __len__(self):
+        return (self.bottom_right.x - self.top_left.x + 1) * (
+            self.bottom_right.y - self.top_left.y + 1
+        )
+
+
+@dataclass(frozen=True)
+class Neighborhood:
+    inner: Extent
+
+    @cached_property
+    def outer(self) -> Extent:
+        return Extent(
+            top_left=Coordinate(
+                x=(self.inner.top_left.x - 1), y=(self.inner.top_left.y - 1)
+            ),
+            bottom_right=Coordinate(
+                x=(self.inner.bottom_right.x + 1), y=(self.inner.bottom_right.y + 1)
+            ),
+        )
+
+    def __contains__(self, item: Coordinate) -> bool:
+        return item in self.outer and not item in self.inner
+
+    def __len__(self):
+        return len(self.outer) - len(self.inner)
+
+
+@dataclass(frozen=True)
+class Symbol:
+    value: str
+    location: Coordinate
+
+
+@dataclass(frozen=True)
+class Number:
+    value: str
+    location: Coordinate
+
+    @cached_property
+    def extent(self):
+        return Extent(
+            self.location,
+            Coordinate(self.location.x + len(self.value) - 1, self.location.y),
+        )
+
+
+@dataclass
+class Schematic:
+    extent: Extent
+    symbols: set[Symbol] = field(default_factory=set)
+    numbers: set[Number] = field(default_factory=set)
+
+
+def load(data_file: Path):
+    symbols = set()
+    numbers = set()
+    with open(data_file) as lines:
+        for y, line in enumerate(lines):
+            for symbol_match in re.finditer(r"(?![\d\\.]).", line.strip()):
+                x = symbol_match.start()
+                value = symbol_match.group()
+                symbols.add(Symbol(value=value, location=Coordinate(x, y)))
+
+            for number_match in re.finditer(r"\d+", line.strip()):
+                x = number_match.start()
+                value = number_match.group()
+                numbers.add(Number(value=value, location=Coordinate(x, y)))
+
+        extent = Extent(
+            top_left=Coordinate(0, 0),
+            bottom_right=Coordinate(x=(len(line) - 1), y=y),
+        )
+
+    return Schematic(extent=extent, symbols=symbols, numbers=numbers)
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 
-from src.day03 import load
+from src.day03 import load, Symbol, Coordinate, Number, Extent
 
 
 class Day03Tests(unittest.TestCase):
@@ -12,6 +12,63 @@ class Day03Tests(unittest.TestCase):
 
     def test_load_example(self):
         self.assertEqual(
-            self.example,
-            [],
+            self.example.symbols,
+            set(
+                [
+                    Symbol(value="*", location=Coordinate(x=3, y=1)),
+                    Symbol(value="#", location=Coordinate(x=6, y=3)),
+                    Symbol(value="*", location=Coordinate(x=3, y=4)),
+                    Symbol(value="+", location=Coordinate(x=5, y=5)),
+                    Symbol(value="$", location=Coordinate(x=3, y=8)),
+                    Symbol(value="*", location=Coordinate(x=5, y=8)),
+                ]
+            ),
         )
+        self.assertEqual(
+            self.example.numbers,
+            set(
+                [
+                    Number(value="467", location=Coordinate(x=0, y=0)),
+                    Number(value="114", location=Coordinate(x=5, y=0)),
+                    Number(value="35", location=Coordinate(x=2, y=2)),
+                    Number(value="633", location=Coordinate(x=6, y=2)),
+                    Number(value="617", location=Coordinate(x=0, y=4)),
+                    Number(value="58", location=Coordinate(x=7, y=5)),
+                    Number(value="592", location=Coordinate(x=2, y=6)),
+                    Number(value="755", location=Coordinate(x=6, y=7)),
+                    Number(value="664", location=Coordinate(x=1, y=9)),
+                    Number(value="598", location=Coordinate(x=5, y=9)),
+                ]
+            ),
+        )
+
+    def test_extent(self):
+        number = Number(value="467", location=Coordinate(x=0, y=0))
+        self.assertTrue(Coordinate(x=0, y=0) in number.extent)
+        self.assertTrue(Coordinate(x=1, y=0) in number.extent)
+        self.assertTrue(Coordinate(x=2, y=0) in number.extent)
+        self.assertEqual(3, len(number.extent))
+
+    def test_extent_iter(self):
+        extent = Extent(
+            top_left=Coordinate(x=0, y=0), bottom_right=Coordinate(x=2, y=1)
+        )
+        self.assertEqual(
+            {
+                Coordinate(x=0, y=0),
+                Coordinate(x=1, y=0),
+                Coordinate(x=2, y=0),
+                Coordinate(x=0, y=1),
+                Coordinate(x=1, y=1),
+                Coordinate(x=2, y=1),
+            },
+            {c for c in extent},
+        )
+
+    def test_neighborhood(self):
+        neighborhood = self.example.extent.neighborhood
+        self.assertEqual(44, len(neighborhood))
+        self.assertTrue(Coordinate(x=-1, y=-1) in neighborhood)
+        self.assertEqual(Coordinate(x=-1, y=-1), neighborhood.outer.top_left)
+        self.assertTrue(Coordinate(x=10, y=10) in neighborhood)
+        self.assertEqual(Coordinate(x=10, y=10), neighborhood.outer.bottom_right)
