80656d73545dcbfbacd691c1c313725ec784a29b
Tyson McNulty <tyson.mcnulty@gmail.com>
2023-12-11 13:17:46 -0500

    Wow, solution 2

--- src/day05/__init__.py
+++ src/day05/__init__.py
@@ -1,6 +1,7 @@
 import re
 from dataclasses import dataclass, field
 from enum import StrEnum, auto
+from itertools import chain
 from pathlib import Path
 from typing import ClassVar, Self
 
@@ -27,14 +28,69 @@ class Unit:
         start: int
         length: int
 
+        @property
+        def end(self):
+            return self.start + self.length
+
+        @property
+        def last(self):
+            return self.end - 1
+
+        def __contains__(self, item: int):
+            return item in range(self.start, self.end)
+
+        def split(self, other):
+            if not isinstance(other, Unit.Range):
+                raise TypeError()
+
+            if not self.category == other.category:
+                return [self]
+
+            if self.start in other:
+                if self.last in other:
+                    return [self]
+                if self.last not in other:
+                    return [
+                        Unit.Range(self.category, self.start, other.end - self.start),
+                        Unit.Range(self.category, other.end, self.end - other.end),
+                    ]
+
+            if other.start in self:
+                if other.last in self:
+                    overlap = other
+                    remainder = [
+                        Unit.Range(self.category, self.start, other.start - self.start)
+                    ]
+                    if self.last > other.last:
+                        remainder.append(
+                            Unit.Range(self.category, other.end, self.end - other.end)
+                        )
+                    return [overlap, *remainder]
+                if other.last not in self:
+                    return [
+                        Unit.Range(self.category, other.start, self.end - other.start),
+                        Unit.Range(self.category, self.start, other.start - self.start),
+                    ]
+
+            return [None, self]
+
+
 @dataclass
 class Almanac:
     maps: dict[Category, "Almanac.Map"]
 
-    def resolve(self, unit: Unit) -> Unit:
+    def resolve_unit(self, unit: Unit) -> Unit:
         _next = unit
         while _next.category in self.maps:
             _next = self.maps[_next.category][_next]
+
+        return _next
+
+    def resolve_range(self, unit_range: Unit.Range) -> set[Unit.Range]:
+        _next = {unit_range}
+        while (category := next(iter(_next)).category) in self.maps:
+            category_map = self.maps[category]
+            _next = set(chain.from_iterable(category_map[r] for r in _next))
 
         return _next
 
@@ -70,22 +126,58 @@ class Almanac:
                 )
 
             if isinstance(item, Unit):
-                unit = item
+                source_unit = item
                 for line in self.lines:
-                    if unit.id in line.source_range:
+                    if source_unit.id in line.source_range:
                         return Unit(
                             category=self.destination,
                             id=(
-                                unit.id
+                                source_unit.id
                                 - line.source_range_start
                                 + line.destination_range_start
                             ),
                         )
 
-                return Unit(category=self.destination, id=unit.id)
+                return Unit(category=self.destination, id=source_unit.id)
 
             if isinstance(item, Unit.Range):
-                return item
+                destination_ranges = set()
+                remainders = {item}
+                for line in self.lines:
+                    line_source_range = Unit.Range(
+                        self.source, line.source_range_start, line.range_length
+                    )
+                    next_remainders = set()
+                    overlapped_remainders = set()
+                    for r in remainders:
+                        [overlap, *line_remainders] = r.split(line_source_range)
+                        if overlap is not None:
+                            overlapped_remainders.add(r)
+                            destination_ranges.add(
+                                Unit.Range(
+                                    category=self.destination,
+                                    start=(
+                                        overlap.start
+                                        - line.source_range_start
+                                        + line.destination_range_start
+                                    ),
+                                    length=overlap.length,
+                                )
+                            )
+
+                        next_remainders |= set(line_remainders)
+
+                    remainders |= next_remainders
+                    remainders -= overlapped_remainders
+
+                destination_ranges.update(
+                    map(
+                        lambda r: Unit.Range(self.destination, r.start, r.length),
+                        remainders,
+                    )
+                )
+
+                return destination_ranges
 
 
 def load(data_file: Path):
@@ -93,7 +185,10 @@ def load(data_file: Path):
         seed_data, *almanac_maps_data = re.split(r"\n\n+", data.read())
         seed_numbers = [int(match) for match in re.findall(r"\d+", seed_data)]
         seeds = [Unit(Category.SEED, id=n) for n in seed_numbers]
-        seed_ranges = [Unit.Range(Category.SEED, seed_numbers[n], seed_numbers[n + 1]) for n in range(0, len(seed_numbers), 2)]
+        seed_ranges = [
+            Unit.Range(Category.SEED, seed_numbers[n], seed_numbers[n + 1])
+            for n in range(0, len(seed_numbers), 2)
+        ]
 
         almanac_maps = []
         for almanac_map_data in almanac_maps_data:
--- tests/test_day05.py
+++ tests/test_day05.py
@@ -1,5 +1,6 @@
 import unittest
 from collections import Counter
+from itertools import chain
 from pathlib import Path
 
 from src.day05 import load, Unit, Category, Almanac
@@ -26,7 +27,7 @@ class Day05Tests(unittest.TestCase):
                 Unit.Range(Category.SEED, 79, 14),
                 Unit.Range(Category.SEED, 55, 13),
             ],
-            self.example[1]
+            self.example[1],
         )
         self.assertEqual(
             self.example[2].maps["seed"].lines,
@@ -47,7 +48,7 @@ class Day05Tests(unittest.TestCase):
         self.assertEqual(Unit(Category.SOIL, 57), almanac_map[Unit(Category.SEED, 55)])
         self.assertEqual(Unit(Category.SOIL, 13), almanac_map[Unit(Category.SEED, 13)])
 
-    def test_resolve(self):
+    def test_resolve_unit(self):
         (seeds, _, almanac) = self.example
         self.assertEqual(
             [
@@ -56,12 +57,108 @@ class Day05Tests(unittest.TestCase):
                 Unit(Category.LOCATION, 86),
                 Unit(Category.LOCATION, 35),
             ],
-            [almanac.resolve(s) for s in seeds],
+            [almanac.resolve_unit(s) for s in seeds],
         )
 
     def test_solution_1(self):
         (seeds, _, almanac) = self.input
         self.assertEqual(
             Unit(Category.LOCATION, 196167384),
-            min([almanac.resolve(s) for s in seeds], key=(lambda loc: loc.id)),
+            min([almanac.resolve_unit(s) for s in seeds], key=(lambda loc: loc.id)),
+        )
+
+    def test_split(self):
+        first = Unit.Range(Category.SEED, 10, 10)
+        second = Unit.Range(Category.SEED, 15, 20)
+        [overlap, *remainder] = first.split(second)
+
+        self.assertEqual(Unit.Range(Category.SEED, 15, 5), overlap)
+        self.assertEqual([Unit.Range(Category.SEED, 10, 5)], remainder)
+
+    def test_split_2(self):
+        first = Unit.Range(Category.SEED, 15, 10)
+        second = Unit.Range(Category.SEED, 10, 10)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 15, 5), overlap)
+        self.assertEqual([Unit.Range(Category.SEED, 20, 5)], remainder)
+
+    def test_split_3(self):
+        first = Unit.Range(Category.SEED, 15, 15)
+        second = Unit.Range(Category.SEED, 20, 5)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 20, 5), overlap)
+        self.assertEqual(
+            [Unit.Range(Category.SEED, 15, 5), Unit.Range(Category.SEED, 25, 5)],
+            remainder,
+        )
+
+    def test_split_4(self):
+        first = Unit.Range(Category.SEED, 15, 15)
+        second = Unit.Range(Category.SEED, 20, 10)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 20, 10), overlap)
+        self.assertEqual([Unit.Range(Category.SEED, 15, 5)], remainder)
+
+    def test_split_5(self):
+        first = Unit.Range(Category.SEED, 15, 5)
+        second = Unit.Range(Category.SEED, 10, 15)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 15, 5), overlap)
+        self.assertEqual([], remainder)
+
+    def test_split_6(self):
+        first = Unit.Range(Category.SEED, 15, 5)
+        second = Unit.Range(Category.SEED, 15, 10)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 15, 5), overlap)
+        self.assertEqual([], remainder)
+
+    def test_split_7(self):
+        first = Unit.Range(Category.SEED, 45, 10)
+        second = Unit.Range(Category.SEED, 50, 48)
+        [overlap, *remainder] = first.split(second)
+        self.assertEqual(Unit.Range(Category.SEED, 50, 5), overlap)
+        self.assertEqual([Unit.Range(Category.SEED, 45, 5)], remainder)
+
+    def test_map_seed_range(self):
+        seed_range = Unit.Range(Category.SEED, 0, 100)
+        seed_to_soil_map = self.example[2].maps["seed"]
+
+        self.assertEqual(
+            {
+                Unit.Range(Category.SOIL, 0, 50),
+                Unit.Range(Category.SOIL, 50, 2),
+                Unit.Range(Category.SOIL, 52, 48),
+            },
+            seed_to_soil_map[seed_range],
+        )
+
+    def test_resolve_range(self):
+        (_, seed_ranges, almanac) = self.example
+        location_ranges = {
+            *chain.from_iterable(almanac.resolve_range(r) for r in seed_ranges)
+        }
+        self.assertEqual(
+            {
+                Unit.Range(category=Category.LOCATION, start=60, length=1),
+                Unit.Range(category=Category.LOCATION, start=82, length=3),
+                Unit.Range(category=Category.LOCATION, start=46, length=10),
+            }
+            | {
+                Unit.Range(category=Category.LOCATION, start=94, length=3),
+                Unit.Range(category=Category.LOCATION, start=97, length=2),
+                Unit.Range(category=Category.LOCATION, start=86, length=4),
+                Unit.Range(category=Category.LOCATION, start=56, length=4),
+            },
+            location_ranges,
+        )
+
+    def test_solution_2(self):
+        (_, seed_ranges, almanac) = self.input
+        location_ranges = {
+            *chain.from_iterable(almanac.resolve_range(r) for r in seed_ranges)
+        }
+        self.assertEqual(
+            Unit.Range(Category.LOCATION, 125742456, 20229261),
+            min(location_ranges, key=(lambda loc_range: loc_range.start)),
         )
